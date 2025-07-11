c1f7d6c6901aaff0b0bc5ba13eaca2f55e3f64fd
Degisew <jegisew21@gmail.com>
2025-07-11 21:52:21 +0300

    feat: implemented simple ReAct agent

---
+++ agents/linkedin_lookup.py
@@ -0,0 +1,33 @@
+from langchain import hub
+from langchain_core.prompts import PromptTemplate
+from langchain.agents import (
+    create_react_agent,
+    AgentExecutor
+)
+
+from .agent_tools import search_tool
+
+
+def ReAct_agent(llm):
+    full_name = "Degisew Mengist Python Developer"
+
+    template = """
+    Given the person full name {full_name}, I want you to give me the linkedin url.
+    Please Note that your answer should only contain the URL.
+    """
+
+    prompt_template = PromptTemplate(
+        input_variables=["full_name"], template=template)
+
+    tools = [search_tool]
+
+    react_prompt = hub.pull("hwchase17/react")
+
+    react_agent = create_react_agent(llm, tools=tools, prompt=react_prompt)
+
+    executor = AgentExecutor(agent=react_agent, tools=tools, verbose=True)
+
+    res = executor.invoke(
+        {"input": prompt_template.format_prompt(full_name=full_name)})
+
+    return res["output"]
